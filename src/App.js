8d03dfc9b18d38be65d4c9f686eea6e98fbaec66
TheSh4h <hh.haseebshah@gmail.com>
2023-08-08 02:27:09 -0700

    lec 4 done

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import {
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Contact from './pages/help/Contact';
+import Faq from './pages/help/Faq';
 
 import RootLayout from "./layout/RootLayout";
+import HelpLayout from "./layout/HelpLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
             <Route path="/" element={<RootLayout />}>
               <Route index element={<Home />} />
               <Route path="about" element={<About />} />
+              <Route path="help" element={<HelpLayout />}>
+                <Route path="contact" element={<Contact />} />
+                <Route path="faq" element={<Faq />} />
+              </Route>
             </Route>
   )
 )
--- src/layout/RootLayout.js
+++ src/layout/RootLayout.js
@@ -7,6 +7,7 @@ const RootLayout = () => {
                 <h1>Jobarouter</h1>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="about">About</NavLink>
+                <NavLink to="help">Help</NavLink>
                 </nav>
             </header>
 
